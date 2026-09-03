31d5996801f6a4055660c05a871e079536488486
rpgunasekara <ravipgunasekara@gmail.com>
2026-09-02 22:09:50 -0400

    put html files in root so github can deploy

--- build.py
+++ build.py
@@ -5,7 +5,6 @@ import markdown
 # get filepath info
 website = sys.argv[1]
 md_dir = os.path.join(website, "markdown")
-html_dir = os.path.join(website, "html")
 
 # convert .md to .html
 for filename in os.listdir(md_dir):
@@ -17,7 +16,7 @@ for filename in os.listdir(md_dir):
 
         html = markdown.markdown(text)
         html_filename = filename[:-3] + ".html"
-        html_file = os.path.join(html_dir, html_filename)
+        html_file = os.path.join(website, html_filename)
 
         with open(html_file, "w") as file:
             file.write(html)
